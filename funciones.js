2c3c69648cd5abdff05a77f83309a54d8cb2e53e
JIweb <81446210+Joel-coder-prog@users.noreply.github.com>
2023-09-14 01:03:38 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function toggleElement() {
+  var elemento = document.getElementById("mi-elemento");
+  if (elemento.style.display === "block") {
+    elemento.style.display = "none";
+  } else {
+    elemento.style.display = "block";
+  }
+}
+
+
+
+
+
